2812e7558ac1359d92ca9fe5b7a565689585b75d
Bartek Kucypera <bartek.kucypera@gazeta.pl>
2024-02-03 10:02:11 +0100

    version 1.0

---
+++ config.h
@@ -0,0 +1,37 @@
+// Copyright 2023 Thomas Haukland (@tompi)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define WS2812_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the WS2812 implementation uses the PIO0 peripheral
+//#define WS2812_TRST_US 80
+#define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
+#define RGB_MATRIX_DEFAULT_VAL 32
+
+
+// Pick good defaults for enabling homerow modifiers
+#define TAPPING_TERM 230
+
+
+#define WS2812_DI_PIN GP16 // The pin connected to the data pin of the LEDs
+ #define RGBLED_NUM 1                     // The number of LEDs connected
+
+
+#define MAX_DEFERRED_EXECUTORS 32
